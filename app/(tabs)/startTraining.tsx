b3fe085f17d1b0af6200362ed1fee364f7f6d0ae
Elias Frick <elias.frick.04@gmail.com>
2024-12-23 19:35:17 +0100

    New Add Icons

--- app/(tabs)/startTraining.tsx
+++ app/(tabs)/startTraining.tsx
@@ -1,71 +1,93 @@
 import React, { useState, useLayoutEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Animated } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Animated,
+} from "react-native";
 import FeatherIcons from "@expo/vector-icons/Feather";
 import { useNavigation } from "@react-navigation/native";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import {
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+  Plus,
+} from "@tamagui/lucide-icons";
+import type { PopoverProps } from "tamagui";
+import {
+  Adapt,
+  Button,
+  Input,
+  isWeb,
+  Label,
+  Popover,
+  XStack,
+  YStack,
+} from "tamagui";
+import { AddDropOver } from "@/components/ui/AddDropOver";
 
 export default function StartTrainingScreen() {
-  const navigation = useNavigation(); // Zugriff auf die Navigation
+  const navigation = useNavigation();
+  const [shouldAdapt, setShouldAdapt] = useState(true);
 
-  // State für die Animation und das Anzeigen der neuen Icons
   const [isOpen, setIsOpen] = useState(false);
-  const translateY = new Animated.Value(0); // Initiale Position für die Animation
+  const translateY = new Animated.Value(0);
 
-  // Neue Optionen, die erscheinen sollen
   const newIcons = [
-    { id: "1", name: "plus", description: "Create own plan", onPress: () => addCustomPlan() },
-    { id: "2", name: "archive", description: "Choose a template", onPress: () => addCustomPlan()  },
-    { id: "3", name: "robot", description: "Create plan with AI",  onPress: () => addCustomPlan()  },
+    {
+      id: "1",
+      name: "plus",
+      description: "Create own plan",
+      onPress: () => addCustomPlan(),
+    },
+    {
+      id: "2",
+      name: "archive",
+      description: "Choose a template",
+      onPress: () => addCustomPlan(),
+    },
+    {
+      id: "3",
+      name: "robot",
+      description: "Create plan with AI",
+      onPress: () => addCustomPlan(),
+    },
   ];
 
-  // Animation starten, um die Icons nach unten zu bewegen
   const toggleIcons = () => {
     setIsOpen((prev) => !prev);
 
     Animated.timing(translateY, {
-      toValue: isOpen ? 0 : 100, // Wenn geöffnet, bringe die Icons nach unten
-      duration: 300,
+      toValue: isOpen ? 0 : 100,
+      duration: 5000,
       useNativeDriver: true,
     }).start();
   };
 
-  // Setze den Header mit Icon
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: "Start Training",
       headerRight: () => (
-        <TouchableOpacity
-          onPress={toggleIcons} // Toggle die Icons bei Klick
-          style={styles.headerIconContainer}
-        >
-          <FeatherIcons name="plus" size={32} color="#fff" />
-        </TouchableOpacity>
+        <View style={{ marginRight: 10 }}>
+          <AddDropOver />
+        </View>
       ),
       headerStyle: {
         backgroundColor: "#F86E51",
       },
-      headerTintColor: "#fff", // Farbe des Titels und Icons
+      headerTintColor: "#fff",
     });
   }, [navigation, isOpen]);
 
   const addCustomPlan = () => {
-    console.log("Test")
-  }
+    console.log("Test");
+  };
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.iconContainer, { transform: [{ translateY }] }]}>
-        {isOpen &&
-          newIcons.map((icon) => (
-            <TouchableOpacity key={icon.id} style={styles.iconItemContainer} onPress={icon.onPress}>
-              <Text style={styles.iconDescription}>{icon.description}</Text>
-              <View style={styles.iconButtonContainer}>
-                <FontAwesome5 name={icon.name} size={32} color="#fff" />
-              </View>
-            </TouchableOpacity>
-          ))}
-      </Animated.View>
-
       <Text style={styles.emptyText}>Create a Workout Routine before</Text>
     </View>
   );
